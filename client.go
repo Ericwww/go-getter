e36eb9868869ef2eb643b928cd1e725ba8a77471
Syasusu <syasusu@163.com>
2024-02-21 20:39:27 +0100

    use %w to wrap error

--- client.go
+++ client.go
@@ -10,8 +10,9 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/hashicorp/go-safetemp"
+
 	urlhelper "github.com/hashicorp/go-getter/helper/url"
-	safetemp "github.com/hashicorp/go-safetemp"
 )
 
 // ErrSymlinkCopy means that a copy of a symlink was encountered on a request with DisableSymlinks enabled.
@@ -205,7 +206,7 @@ func (c *Client) Get() error {
 		td, err := ioutil.TempDir("", "getter")
 		if err != nil {
 			return fmt.Errorf(
-				"Error creating temporary directory for archive: %s", err)
+				"Error creating temporary directory for archive: %w", err)
 		}
 		defer os.RemoveAll(td)
 
@@ -220,7 +221,7 @@ func (c *Client) Get() error {
 	// Determine checksum if we have one
 	checksum, err := c.extractChecksum(u)
 	if err != nil {
-		return fmt.Errorf("invalid checksum: %s", err)
+		return fmt.Errorf("invalid checksum: %w", err)
 	}
 
 	// Delete the query parameter if we have it.
@@ -320,7 +321,7 @@ func (c *Client) Get() error {
 		// if we're specifying a subdir.
 		err := g.Get(dst, u)
 		if err != nil {
-			err = fmt.Errorf("error downloading '%s': %s", RedactURL(u), err)
+			err = fmt.Errorf("error downloading '%s': %w", RedactURL(u), err)
 			return err
 		}
 	}
